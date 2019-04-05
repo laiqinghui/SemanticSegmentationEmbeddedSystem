7c76a1659dfcfb476b06a6b41535ff77cf2d8721
Qing Hui <qinghui1994@gmail.com>
2019-04-05 10:41:42 +0800

    Added test squeezenet out

--- squeeznet/hello.cpp
+++ squeeznet/hello.cpp
@@ -1,6 +1,22 @@
-#include "stdio.h"
+// 'Hello World!' program 
  
-int main(void) {
-  printf("Hello world !\n");
+#include <iostream>
+ 
+int main(int argc, char** argv)
+{
+   if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        return -1;
+    }
+
+    const char* imagepath = argv[1];
+
+    cv::Mat m = cv::imread(imagepath, 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        return -1;
+    }
   return 0;
 }
---
+++ squeeznet/testcv.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        return -1;
+    }
+
+    const char* imagepath = argv[1];
+
+    cv::Mat m = cv::imread(imagepath, 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        return -1;
+    }
+  return 0;
+}
